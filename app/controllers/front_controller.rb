7f09fb8ece2c80835068eb8508e9b20a48427d22
kevin <zzpf@163.com>
2014-03-03 10:43:20 +0800

    region articles

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -6,6 +6,7 @@ class FrontController < ApplicationController
 
   def region
     @categories = Category.all
+    @segments = Segment.all
     @province = Province.find_by_abbr(request.subdomain) || not_found
 #   @province = Province.find_by_abbr(params[:abbr])
     #@user = User.where(:name => request.subdomain).first || not_found
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ActiveRecord::Base
   attr_accessible :content, :segment_id, :province_id, :title
   belongs_to :segment
   belongs_to :province
+
+  scope :published, where( published: true)
+  scope :region, lambda { |id| where( province_id: id) } 
 end
