0aa4fc5c5b3218edc1aa963b711c78deb8108695
kevin.zhou <zzpf@163.com>
2014-03-12 20:30:42 +0800

    search form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,14 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.search(params[:search])
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
     end
   end
+
 
   # GET /posts/1
   # GET /posts/1.json
