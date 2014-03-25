63529bc48348d5e2a8647e7d0b52a866c27084ad
zzpf <zzpf2007@gmail.com>
2014-03-25 22:58:39 +0800

    sort 2

--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -1,2 +1,22 @@
 module FrontHelper
+  def build_link(province, category, sub_category)
+    link = ""
+    if not province.nil?
+      link = '/' + province.abbr
+    end
+
+    if not category.nil?
+      link = link + '/jzc' + category.id.to_s
+    else
+      link = link + '/jzc0'
+    end
+
+    if not sub_category.nil?
+      link = link + 's' + sub_category.id.to_s
+    else
+      link = link + 's0'
+    end
+
+    link = link + 'p0.html'
+  end
 end
